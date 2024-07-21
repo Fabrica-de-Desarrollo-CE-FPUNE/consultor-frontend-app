dd74f20d4da74c755e8d405cff205ebf20580fd0
alejandromtz619 <alejandro.zenitram619@gmail.com>
2024-07-21 19:10:36 -0400

    perfil de usuario

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,16 +7,69 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Perfil de usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+      <IonContent fullscreen={true} className='ion-padding'>
+      
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Usuario</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonItem>
+              <IonLabel position= "floating">Nombre del usuario</IonLabel>
+              <IonTextarea readonly placeholder='Aquí contenemos el nombre por ejemplo: Alejandro Martinez'></IonTextarea>
+              <IonLabel position= "floating">Número de cédula de Identidad</IonLabel>
+              <IonTextarea readonly placeholder='Aquí contenemos el ci, por ejemplo: 5144324'></IonTextarea>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Contacto</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonItem>
+              <IonLabel position= "floating">Email</IonLabel>
+              <IonTextarea readonly placeholder='Aquí contenemos el correo por ejemplo: fulano@outlook.com'></IonTextarea>
+              <IonLabel position= "floating">Teléfono Particular</IonLabel>
+              <IonTextarea readonly placeholder='Aquí contenemos el teléfono de línea baja por ejemplo: 021-111-222'></IonTextarea>
+              <IonLabel position= "floating">Celular</IonLabel>
+              <IonTextarea readonly placeholder='Aquí contenemos el teléfono particular por ejemplo: 0981-111222'></IonTextarea>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Tiempo y Rendimiento Académico</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            <IonItem>
+              <IonLabel position= "floating">Carrera</IonLabel>
+              <IonTextarea readonly placeholder='Información aquí'></IonTextarea>
+              <IonLabel position= "floating">Fecha de ingreso</IonLabel>
+              <IonTextarea readonly placeholder='Fecha aquí'></IonTextarea>
+              <IonLabel position= "floating">Fecha estimada de egreso</IonLabel>
+              <IonTextarea readonly placeholder='Fecha aquí'></IonTextarea>
+              <IonLabel position= "floating">Promedio</IonLabel>
+              <IonTextarea readonly placeholder='Número decimal aquí'></IonTextarea>
+              <IonLabel position= "floating">Total Materias Aprobadas</IonLabel>
+              <IonTextarea readonly placeholder='Número entero aquí'></IonTextarea>
+              <IonLabel position= "floating">Total Materias Reprobadas</IonLabel>
+              <IonTextarea readonly placeholder='Número entero aquí'></IonTextarea>
+              <IonLabel position= "floating">Porcentaje Materias Reprobadas</IonLabel>
+              <IonTextarea readonly placeholder='Porcentaje de regla de tres [(100 * materias en total)/materias reprobadas]'></IonTextarea>
+            </IonItem>
+          </IonCardContent>
+        </IonCard>
+        
       </IonContent>
     </IonPage>
   );
