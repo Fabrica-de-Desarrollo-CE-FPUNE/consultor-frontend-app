c9f016cb84c351f1910800b46d8ba38ca409ce04
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-07-30 22:01:35 -0400

    feat: Agregar nueva interfaz CustomInputHTMLAttributes
    - Se ha importado `IonInput` desde "@ionic/react".
    - Se ha agregado la nueva interfaz `CustomInputHTMLAttributes`, que extiende las propiedades de `IonInput` y añade un estado personalizado:
      - `state: { value: any; reset: (newValue: React.SetStateAction<any>) => void; onIonChange: any; onKeyUp: any; }`
    - Esta interfaz permitirá manejar estados personalizados en `IonInput`.

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,4 +1,15 @@
+import { IonInput } from "@ionic/react";
+
 export interface ErrorMessage {
     id:string,
     message:string
+}
+
+export interface CustomInputHTMLAttributes extends React.ComponentProps<typeof IonInput>{
+    state: {
+        value: any;
+        reset: (newValue: React.SetStateAction<any>) => void;
+        onIonChange: any;
+        onKeyUp: any;
+    }
 }
