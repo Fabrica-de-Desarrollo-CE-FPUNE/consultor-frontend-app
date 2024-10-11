f866c2202f2b5ed13b213178c4d69934b683204b
David Delvalle <ddelvallerojas9@gmail.com>
2024-10-11 15:00:13 -0300

    fix: extraccion de la alguna que contenga la caché sobre todaInfo

--- src/data/TodaLaInfoStore.ts
+++ src/data/TodaLaInfoStore.ts
@@ -7,9 +7,27 @@ interface TodaLaInfoStoreState {
     todo: TodaLaInfo|null|undefined
 }
 
-export const TodaLaInfoStore = new Store<TodaLaInfoStoreState>({
-    todo: JSON.parse((await Preferences.get({key:'todaInfo'})).value??'{}' ) as TodaLaInfo
-});
+const retornarInfo = async ()=>{
+
+    const get = await Preferences.get({key:'todaInfo'}).then((data)=>{
+        if(data && data.value){
+            return data.value as unknown as TodaLaInfo
+        }
+        return null;
+    }).catch((err)=>{
+        console.log(err);
+        return null;
+    });
+
+    const resultado:TodaLaInfoStoreState = {
+        todo: get
+    }
+
+    return resultado
+}
+
+
+export const TodaLaInfoStore = new Store<TodaLaInfoStoreState>(await retornarInfo());
 
 export const vaciarTodaLaInfo = async ()=>{
     TodaLaInfoStore.update(s=>{
