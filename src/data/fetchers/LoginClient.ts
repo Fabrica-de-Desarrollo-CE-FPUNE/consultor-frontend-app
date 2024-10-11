84cf09f5ac1121ea7e68e374fc21fdd7fecfee85
David Delvalle <ddelvallerojas9@gmail.com>
2024-10-11 11:27:05 -0300

    feact(fetchers): creacion de LoginClient.ts.
    - Clase que extendida de ApiClient.ts usando la interface de TodaLaInfo.

---
+++ src/data/fetchers/LoginClient.ts
@@ -0,0 +1,18 @@
+import { ApiClient } from "./ApiClient";
+import { TodaLaInfo } from "../types";
+
+export class LoginClient extends ApiClient<TodaLaInfo> {
+
+    constructor() {
+        super('https://bqs-service-dev-pr-35.onrender.com/api/estudiante')
+    }
+
+    protected handleStatus(status: number): void {
+        if (status >= 400 && status < 500) {
+            console.warn(`Error del cliente: ${status}`);
+        } else if (status >= 500) {
+            console.error(`Error del servidor: ${status}`);
+        }
+    }
+    
+}
