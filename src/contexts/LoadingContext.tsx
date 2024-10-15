66a265cba8edbb9f2ed1b2c94b9b28070a576143
David Delvalle <ddelvallerojas9@gmail.com>
2024-10-15 18:02:01 -0300

    refactor: separacion de responsabilidades en LoadingContext.tsx

--- src/contexts/LoadingContext.tsx
+++ src/contexts/LoadingContext.tsx
@@ -1,46 +1,7 @@
-import React, { useState, createContext, ReactNode } from 'react';
-import './LoadingContext.css';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-import { IonCol, IonGrid } from '@ionic/react';
+import  {createContext} from 'react';
 
-// Crear el contexto con un valor predeterminado
 export const LoadingContext = createContext({
   estaCargando: false,
   setEstaCargando: (estaCargando: boolean) => {},
 });
 
-interface Props {
-  children: ReactNode;
-}
-
-// Crear un proveedor de contexto
-export const LoadingProvider: React.FC<Props> = ({ children }) => {
-  const [estaCargando, setEstaCargando] = useState(false);
-
-  return (
-    <LoadingContext.Provider value={{ estaCargando, setEstaCargando }}>
-      {children}
-      {estaCargando && (
-        <div className="loader-overlay">
-            <IonGrid>
-                <IonCol size='12'>
-                    <DotLottieReact
-                    src="lotties/logo.json"
-                    loop
-                    autoplay
-                    />
-                </IonCol>
-                <IonCol push='3' size='6' >
-                    <DotLottieReact
-                        className='ball'
-                        src="lotties/ball.json"
-                        loop
-                        autoplay
-                    />
-                </IonCol>
-            </IonGrid>
-        </div>
-      )}
-    </LoadingContext.Provider>
-  );
-};
