dd4de444b0ab676cfbb62fe382c205c27e61e2ab
David Delvalle <ddelvallerojas9@gmail.com>
2024-12-17 01:46:23 -0300

    feat: creacion de cards para vista de finales.
    - Se creó cards para cuando ya hay nota de finales.
    - Cards para cuando ya existe nota en las calificaciones, que probablemente se trate de una exoneracion.
    - Animacion en la calculadora para poder avisar al usuario que ya deberia de revisar sus calculos, asi como también cambio de colores para enfocar mejor.

--- src/pages/MateriaDetalle.tsx
+++ src/pages/MateriaDetalle.tsx
@@ -1,25 +1,37 @@
-// src/pages/MateriaDetalle.tsx
-
-import React from 'react';
-import { IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar, useIonModal } from '@ionic/react';
+import React, { useEffect } from 'react';
+import { IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonItem, IonItemGroup, IonLabel, IonList, IonPage, IonRow, IonTitle, IonToolbar, useIonAlert, useIonModal } from '@ionic/react';
 import './MateriaDetalle.css';
-import { calculatorOutline } from 'ionicons/icons';
-import { useParams } from 'react-router';
+import { calculatorOutline, downloadOutline } from 'ionicons/icons';
+import { useHistory, useParams } from 'react-router';
 import { TodaLaInfoStore } from '../data/TodaLaInfoStore';
-import { espaciosEntreNumeros, primerasLetrasMayusculas } from '../data/utils';
+import { espaciosEntreNumeros, isResultadoParcialCompleto, primerasLetrasMayusculas, transformarEvaluacion } from '../data/utils';
 import Calculadora from '../components/Calculadora';
+import { AppLauncher } from '@capacitor/app-launcher';
 
 const MateriaDetalle: React.FC = () => {
 
+  const history = useHistory();
   const {name} = useParams<{name:string}>();
- 
+  const [alert] = useIonAlert();
 
   const materiaInscripcion = TodaLaInfoStore.useState(s=> s.todo?.info_inscripciones
     .filter(materiaDetalle=>materiaDetalle.materia.toLowerCase().includes(name.toLowerCase()))[0]
   );
-
+  
   const materiaDesemp = TodaLaInfoStore.useState(s=>s.todo?.info_parciales.filter(materiaDetalle=>
     materiaDetalle.materia.toLowerCase().includes(name.toLowerCase()))[0]);
+
+  const materiaFinales = TodaLaInfoStore.useState(s=>s.todo?.info_finales.filter(materiaFinal=>
+    materiaFinal.materia.toLowerCase().includes(name.toLowerCase()) && materiaFinal.final.length
+  )) || [];
+
+  const materiaCalificaciones = TodaLaInfoStore.useState(s=>s.todo?.info_calificaciones.filter((materiaCalificacion=>
+    materiaCalificacion.materia.toLowerCase().includes(name.toLowerCase())
+  )));
+
+  const evaluacion = transformarEvaluacion(materiaDesemp?.evaluacion||"").map((v,i,a)=>{
+    if(i!==a.length-1) return v;
+  }).filter(v=>v!==undefined);
 
   const cerrar = ()=>{
     return cerrarCalculadora()
@@ -31,11 +43,23 @@ const MateriaDetalle: React.FC = () => {
     cerrar
   });
 
+
+  useEffect(()=>{
+    if(!materiaDesemp) {
+      history.goBack();
+    }
+  },[materiaDesemp])
+
   
   const noIncluir = [
-    'materia'
+    'materia', 'bonificacion', 'semestre'
   ]
 
+  
+  const abrirURL = async () => {
+    await AppLauncher.openUrl({ url: `https://www.fpune.edu.py/web/docs/programas/${materiaDesemp?.materia.substring(0,4)}.pdf`});
+  };
+  
   return (
     <IonPage>
       <IonHeader>
@@ -53,6 +77,7 @@ const MateriaDetalle: React.FC = () => {
               <IonLabel>
                 <h1>Información de {name}</h1>
               </IonLabel>
+              
             </IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
@@ -63,7 +88,7 @@ const MateriaDetalle: React.FC = () => {
                   const valor = materiaInscripcion[parametro];
                   if(valor && !noIncluir.includes(parametro)){
                     return(
-                      <IonCol key={index} sizeXs='6' sizeSm='4' sizeMd='3'>
+                      <IonCol  key={index} sizeXs='6'>
                         <IonLabel color="dark">
                           <h3>{primerasLetrasMayusculas(parametro.replace(/_/g,' '))}</h3>
                           <p>{valor}</p>
@@ -81,7 +106,7 @@ const MateriaDetalle: React.FC = () => {
         <IonCard>
           <IonCardHeader>
             <IonCardTitle>
-              <IonLabel>
+            <IonLabel>
                 <h1>Desempeño</h1>
               </IonLabel>
             </IonCardTitle>
@@ -91,17 +116,17 @@ const MateriaDetalle: React.FC = () => {
               <IonRow>
                 {
                   materiaDesemp && (
-                    Object.keys(materiaDesemp).map((parametro, index)=>{
+                    Object.keys(materiaDesemp).map((parametro, index, array)=>{
                       
                       const titulo = primerasLetrasMayusculas(espaciosEntreNumeros({texto: parametro, ignorarOtrosNumeros:false}).replace(/_/g,' '))
                       const valor = materiaDesemp[parametro]
                       
-                      if(!noIncluir.includes(parametro)){
+                      if(!noIncluir.includes(parametro) && evaluacion.length && evaluacion[index-1]>0  || array.length-1===index ){
                         return (
-                          <IonCol key={index} sizeXs='6' sizeSm='4' sizeMd='3'>
+                          <IonCol key={index} sizeXs='6' size='auto'>
                             <IonLabel color="dark">
                               <h3>{titulo}</h3>
-                              <p>{valor!==""?valor:"0"}</p>
+                              <p>{valor}</p>
                             </IonLabel>
                           </IonCol>
                         )
@@ -113,14 +138,146 @@ const MateriaDetalle: React.FC = () => {
                 }
                 
               </IonRow>
+
             </IonGrid>
           </IonCardContent>
         </IonCard>
+        {
+           materiaFinales && materiaFinales.length>0 && (
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>
+                <IonLabel>
+                    <h1>Resultado de las Finales</h1>
+                  </IonLabel>
+                </IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonList>
+                  <IonItemGroup>
+                    {
+                      materiaFinales.map((materiaFinal, index)=>
+                        (
+                          <IonItem color="light" key={index} className={Number(materiaFinal.total)>=60?'animate__animated animate__headShake animate__repeat-3':''}>
+                            <IonGrid>
+                              <IonRow>
+                              {
+                              Object.keys(materiaFinal).map((parametro, index)=>{
+                      
+                                const titulo = primerasLetrasMayusculas(espaciosEntreNumeros({texto: parametro, ignorarOtrosNumeros:false}).replace(/_/g,' '))
+                                const valor = materiaFinal[parametro]
+                                if(!noIncluir.includes(parametro)){
+                                  return (
+                                      <IonCol key={index}>
+                                        <IonLabel color={Number(materiaFinal.total)>=60?'success':'danger'}>
+                                          <h3>{titulo}</h3>
+                                        </IonLabel>
+                                        <IonLabel color="medium">
+                                          <p>{valor}</p>
+                                        </IonLabel>
+                                      </IonCol>
+                                  )
+                                }
+                                return null
+                              })
+                            }
+                              </IonRow>
+                            </IonGrid>
+                          </IonItem>
+                        )
+                      )            
+                    }
+                  </IonItemGroup>
+                </IonList>
+              </IonCardContent>
+            </IonCard>
+          )
+        }
+        {
+          (materiaCalificaciones && materiaCalificaciones.length>0 && !(materiaFinales && materiaFinales.length>0)) && (
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>
+                <IonLabel>
+                    <h1>Calificaciones Finales</h1>
+                  </IonLabel>
+                </IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonList>
+                  <IonItemGroup>
+                    {
+                      materiaCalificaciones.map((materiaCalificacion, index)=>
+                        (
+                          <IonItem color="light" key={index} className={materiaCalificacion.nota!=='Uno'?'animate__animated animate__headShake animate__repeat-3':''}>
+                            <IonGrid>
+                              <IonRow>
+                              {
+                              Object.keys(materiaCalificacion).map((parametro, index)=>{
+                      
+                                const titulo = primerasLetrasMayusculas(espaciosEntreNumeros({texto: parametro, ignorarOtrosNumeros:false}).replace(/_/g,' '))
+                                const valor = materiaCalificacion[parametro]
+                                if(!noIncluir.includes(parametro)){
+                                  return (
+                                      <IonCol key={index}>
+                                        <IonLabel color={materiaCalificacion.nota!=='Uno'?'success':'danger'}>
+                                          <h3>{titulo}</h3>
+                                        </IonLabel>
+                                        <IonLabel color="medium">
+                                          <p>{valor}</p>
+                                        </IonLabel>
+                                      </IonCol>
+                                  )
+                                }
+                                return null
+                              })
+                            }
+                              </IonRow>
+                            </IonGrid>
+                          </IonItem>
+                        )
+                      )            
+                    }
+                  </IonItemGroup>
+                </IonList>
+              </IonCardContent>
+            </IonCard>
+          )
+        }
+        {
+          !materiaCalificaciones?.length && !materiaFinales.length && (
+            <IonItem>
+              <IonLabel>Aún no hay información sobre tus calificaciones finales en esta materia.</IonLabel>
+            </IonItem>
+          )
+        }
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
-          <IonFabButton onClick={()=>mostrarCalculadora()}>
+          <IonFabButton 
+          color={materiaDesemp && isResultadoParcialCompleto(materiaDesemp,evaluacion)?'success':'danger'} 
+          className={`ion-margin-bottom ${materiaDesemp && isResultadoParcialCompleto(materiaDesemp, evaluacion)?'animate__animated animate__bounce animate__delay-3s animate__repeat-3':''}`}
+          onClick={()=>mostrarCalculadora()}>
             <IonIcon icon={calculatorOutline} />
           </IonFabButton>
+          <IonFabButton color='primary' onClick={()=>{
+            alert('¿Descargar programa de estudios de la materia?',[
+              {
+                text:'Sí',
+                handler: async ()=>{
+                  await abrirURL()
+                },
+                cssClass:'success',
+                
+              },
+              {
+                text:'No',
+                role:'destructive'
+              }
+            ])
+          }}>
+            <IonIcon icon={downloadOutline}/>
+          </IonFabButton>
         </IonFab>
+       
       </IonContent>
     </IonPage>
   );
