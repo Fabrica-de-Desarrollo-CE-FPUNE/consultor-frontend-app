e1739a4e25b633c1a2f5ad6cbdc4ba1043542998
David Delvalle <ddelvallerojas9@gmail.com>
2024-12-17 01:37:54 -0300

    feat: implementacion general de animate.css

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,8 @@ import '@ionic/react/css/display.css';
 import '@ionic/react/css/palettes/dark.class.css'; 
 /*import '@ionic/react/css/palettes/dark.system.css'; */
 
+import 'animate.css';
+
 /* Theme variables */
 import './theme/variables.css';
 import Login from './pages/Login';
